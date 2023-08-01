cc1c9292e8c233f4604755314941e0464f210791
rakesh <rakesh.nallam@clearcapital.com>
2023-07-31 17:37:57 -0700

    fix errors

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -14,7 +14,7 @@ import { isNullOrUndefined } from "~/services/booleanServices";
 const getBaseUrl = () => {
   if (typeof window !== "undefined") return ""; // browser should use relative url
   if (!isNullOrUndefined(process.env.VERCEL_URL))
-    return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
+    return `https://${process.env.VERCEL_URL ?? ""}`; // SSR should use vercel url
   return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
 };
 
