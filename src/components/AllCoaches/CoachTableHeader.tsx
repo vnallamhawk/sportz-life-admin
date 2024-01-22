de38cb0a7606e0fdc4c8e7360bb2f236d9439ac6
Rakesh <rakeshnallam28@gmail.com>
2024-01-21 18:25:34 -0800

    fix css

--- src/components/AllCoaches/CoachTableHeader.tsx
+++ src/components/AllCoaches/CoachTableHeader.tsx
@@ -8,7 +8,7 @@ export default function CoachTableHeader() {
     let component;
     if (id === "selectControl") {
       component = (
-        <Checkbox.Root className="CheckboxRoot" defaultChecked id="c1">
+        <Checkbox.Root className="absolute left-1 top-0 h-full w-full" id="c1">
           <Checkbox.Indicator className="CheckboxIndicator">
             <CheckIcon />
           </Checkbox.Indicator>
@@ -35,7 +35,7 @@ export default function CoachTableHeader() {
       {COACH_TABLE_HEADERS.map(({ id, label }) => (
         <th
           key={id}
-          className="font-['DM Sans'] w-3 rounded-md border-red-100 px-6 px-[3px] py-3 py-[3px] text-left font-medium first:border-2 first:text-center "
+          className="font-['DM Sans'] relative w-3 rounded-md border-red-100 text-left font-medium first:border-2 first:text-center "
         >
           {getRenderComponent(id, label)}
         </th>
