1c92decbfe486abf754c827960709941767f6657
lly02 <lly.02@outlook.com>
2024-01-24 12:38:59 +0800

    feat: side nav icons inserted

--- src/constants/SideNavConstants.ts
+++ src/constants/SideNavConstants.ts
@@ -2,53 +2,66 @@ export const SIDE_NAV_ITEMS = [
   {
     label: "Dashboard",
     route: "/dashboard",
+    icon: "dashboard.svg",
   },
   {
     label: "Centers",
     route: "/centers",
+    icon: "centers.svg",
   },
   {
     label: "Staffs",
     route: "/staff",
+    icon: "staffs.svg",
   },
   {
     label: "Coach",
     route: "/coach",
+    icon: "coach.svg",
   },
   {
     label: "Athlete",
     route: "/athlete",
+    icon: "athelete.svg",
   },
   {
     label: "Assessments",
     route: "/assessments",
+    icon: "assessments.svg",
   },
   {
-    label: "Coaching Drills",
+    label: "Training Drills",
     route: "/drills",
+    icon: "training-drills.svg",
   },
   {
-    label: "Training Plans",
+    label: "Training Plan",
     route: "/plans",
+    icon: "training-plan.svg",
   },
   {
     label: "Competitions",
     route: "/competitions",
+    icon: "competitions.svg",
   },
   {
     label: "Support",
     route: "/support",
+    icon: "support.svg",
   },
   {
     label: "Plan and Pricing",
     route: "/pricing",
+    icon: "plan-and-pricing.svg",
   },
   {
     label: "Post",
     route: "/post",
+    icon: "post.svg",
   },
   {
     label: "Reports",
     route: "/reports",
+    icon: "reports.svg",
   },
 ];
