5c3f70d5c28896043a1b90b2c718e6cebb2a21a4
Gamini Jindal <gamuaggarwal@gmail.com>
2024-07-05 14:02:47 +0530

    tailwind config updated

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,11 +6,58 @@ export default {
     extend: {
       colors: {
         "side-nav-orange": "#ff9678",
+        "theme": {
+          "light" : "#F3476D",
+          "dark" : "#a70025",
+        },
+        "orange" : {
+          "light" : "#FF9678",
+          "dark"  : "#f66238",
+        },
+        "s-gray" : "#F9F9F9",
+        "burgundy" : "#CF8DA7",
+        "mandy" : {
+          "light" : "#F78AA2",
+          "dark" : "#f8436b"
+        },
+        "blush" : "#FFBEAB",
+      
       },
+      maxHeight: {
+        '98vh': '98vh',
+      },
+      minWidth: {
+        '370': '370px',
+        '256' : '256px'
+      },
+      left: {
+        "256" : "-256px"
+      },
+      bottom: {
+        "24" : "-24px"
+      },
+    
     },
+    
     fontFamily: {
       heading: "Teko, sans-serif",
     },
+    screens: {
+      'sm': '576px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '992px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1200px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1440px',
+      // => @media (min-width: 1536px) { ... }
+    }
   },
   plugins: [],
 } satisfies Config;
