c9ee3a8b5bd1affe0a25f79f2d3977b58287721b
Gamini Jindal <gamuaggarwal@gmail.com>
2024-07-19 10:52:57 +0530

    coach screen updated

--- src/pages/coach/AllCoach.tsx
+++ src/pages/coach/AllCoach.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Card from "~/components/Card/Card";
 import CardTitle from "~/components/Card/CardTitle";
 import Textbox from "~/components/Textbox/Textbox";
@@ -9,20 +9,41 @@ import CoachTableBody from "../../components/AllCoaches/CoachTableBody";
 import { useRouter } from "next/navigation";
 import LoadingSpinner from "~/components/LoadingSpinner/LoadingSpinner";
 import DashboardHeader from "~/components/DashboardHeader";
+import AllData from "~/common/AllData";
+import { COACH_TABLE_HEADERS } from "~/constants/coachConstants";
+import { api } from "~/utils/api";
 
 export default function AllCoach() {
   const router = useRouter();
 
   const [filterByName, setFilterByName] = useState("");
   const [loading, setLoading] = useState(true);
-
+  const [finalData, setFinalData] = useState([]);
+ 
   const handleIsLoading = (isLoading: boolean) => {
     setLoading(isLoading);
   };
 
+  const { data: coaches } =
+    filterByName == ""
+      ? api.coach.getAllCoaches.useQuery()
+      : api.coach.getCoachesByName.useQuery({ name: filterByName });
+
+  useEffect(() => {
+    if (coaches && coaches?.length > 0) {
+      const updatedCoaches = coaches.map((coach) => {
+        return {
+          ...coach,
+          batches: coach?.Batches?.length,
+        };
+      });
+      setFinalData(updatedCoaches);
+    }
+  }, [JSON.stringify(coaches)]);
+
   return (
     <>
-      <Card className="h-full">
+      {/* <Card className="h-full">
         <header className="flex justify-between p-2">
           <CardTitle title="ALL COACHES" />
           <div>
@@ -44,7 +65,23 @@ export default function AllCoach() {
           tableBody={CoachTableBody({ name: filterByName }, handleIsLoading)}
         />
         {loading ? <LoadingSpinner /> : ""}
-      </Card>
+      </Card> */}
+    <AllData
+        title="ALL COACHES"
+        addButtonText="ADD NEW COACH"
+        addButtonUrl="/coach/AddCoach"
+        dropdownItems={{reminder:true,freeze:true,changeBatch:true}}
+        filter={false}
+        TABLE_HEAD={COACH_TABLE_HEADERS}
+        TABLE_ROWS={finalData}
+        setFilterByName={setFilterByName}
+        filterByName={filterByName}
+        rowSelection={true}
+        showImage={false}
+        onViewClick={(id)=>router.push(`/coach/${id ?? ""}`)}
+        onEditClick={(id)=>router.push(`/edit-coach-${id}`)}
+      />
+
     </>
   );
 }
