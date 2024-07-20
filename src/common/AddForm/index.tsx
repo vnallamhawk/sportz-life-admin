b66cf5ca1a5e671ad9e533cf440f069d88689a7a
Gamini Jindal <gamuaggarwal@gmail.com>
2024-07-20 13:22:44 +0530

    common add form done

---
+++ src/common/AddForm/index.tsx
@@ -0,0 +1,308 @@
+import React, { useEffect, useContext, useState, useRef } from "react";
+
+import Datepicker from "~/components/DatePicker/DatePickerWrapper";
+import Textbox from "~/components/Textbox";
+import Timepicker from "~/components/TimePicker/TimePickerWrapper";
+import CardTitle from "~/components/Card/CardTitle";
+import Image from "next/image";
+import Plus from "../../images/plus.svg";
+
+import { Controller, useForm } from "react-hook-form";
+import Select from "react-select";
+import Button from "~/components/Button";
+
+const AddForm = ({
+  cardTitle,
+  cardSubTitle,
+  formConstantValues,
+  imageTitle,
+  tableTitle,
+  tableDescription,
+  mobileAddButtonText,
+  TableHeadings,
+  tableData,
+  addTableData,
+  tablekey,
+  buttonItems,
+  setFormData,
+  formData,
+  setCurrentStep,
+  currentStep,
+  finalFormSubmissionHandler
+}) => {
+  let inputElement;
+  const {
+    control,
+    getValues,
+    reset,
+    trigger,
+    formState: { errors },
+  } = useForm<any>({ mode: "onSubmit" });
+
+  const [currentTableData,setCurrentTableData]=useState({})
+
+  const nextClickHandler = async () => {
+    const result = await trigger();
+    if (result) {
+      const currentFormValues = getValues();
+      setFormData && setFormData({ ...formData, ...currentFormValues });
+      setCurrentStep && setCurrentStep(currentStep + 1);
+    }
+  };
+
+  const prevClickHandler = () => {
+    setCurrentStep && setCurrentStep(currentStep - 1);
+  };
+
+  const handleChangeCurrentData=(name,value)=>{
+    let obj={...currentTableData}
+    obj[name]=value
+    setCurrentTableData(obj)
+  }
+
+  const submitCallback = () => {
+    const finalFormData = {
+      ...formData,
+    };
+    if(tablekey){
+        finalFormData[tablekey]=tableData
+    }
+   finalFormSubmissionHandler(finalFormData);
+  };
+
+  const getInputElement = (props) => {
+    const { type, rules, id, pattern, placeHolder } = props;
+    switch (type) {
+      case "select":
+        const { options } = props;
+        inputElement = (
+          <Controller
+            control={control}
+            name={id}
+            rules={rules}
+            render={({ field: { onChange, value } }) => {
+              return (
+                <Select
+                  isMulti={props?.isMulti ?? false}
+                  options={options}
+                  value={value}
+                  placeholder={placeHolder}
+                  className="border-1 c-select w-full border-gray-300"
+                  classNamePrefix="react-select"
+                  onChange={(element) => {
+                    onChange(element);
+                  }}
+                />
+              );
+            }}
+          />
+        );
+        break;
+      case "time":
+        inputElement = (
+          <Controller
+            control={control}
+            render={({ field: { onChange, value } }) => {
+              return (
+                <Timepicker
+                  placeHolder={props.placeHolder}
+                  value={
+                    currentBatchDetail[id] ? currentBatchDetail[id] : "10:00"
+                  }
+                  className="h-12"
+                  onChangeHandler={(value) => handleChangeBatch(value, id)}
+                />
+              );
+            }}
+            name={id}
+            rules={rules}
+          />
+        );
+        break;
+      case "calendar":
+        inputElement = (
+          <Controller
+            control={control}
+            render={({ field: { onChange, value } }) => {
+              return (
+                <Datepicker
+                  placeHolder={props.placeHolder}
+                  value={new Date(value as string)}
+                  className="h-12"
+                  onChangeHandler={onChange}
+                />
+              );
+            }}
+            name={id}
+            rules={rules}
+          />
+        );
+        break;
+      default:
+        inputElement = (
+          <Controller
+            control={control}
+            name={id}
+            render={({ field: { onChange, value } }) => (
+              <Textbox
+                className="h-12 w-full"
+                placeHolder={props.label}
+                onChangeHandler={onChange}
+                // TODO: FIX THIS TS ERROR
+                value={value as string}
+              />
+            )}
+            rules={rules}
+            {...(pattern ? { pattern } : {})}
+          />
+        );
+    }
+
+    return inputElement;
+  };
+
+  return (
+    <>
+      <CardTitle title={cardTitle} />
+      <div className=" text-center font-heading text-3xl font-medium uppercase lg:text-left">
+    {cardSubTitle}
+      </div>
+      <div className="grid-col-1 mt-8 grid gap-x-8 gap-y-4 lg:grid-cols-2 lg:gap-y-8 ">
+        {formConstantValues.map((formValues) => (
+          <div key={formValues.id}>
+            {getInputElement(formValues)}
+
+            <span className="text-red-800">
+              {errors[formValues.id]?.type === "required" && (
+                <div>This field is required</div>
+              )}
+              {errors[formValues.id]?.type === "pattern" && (
+                <div> This field is not matching the pattern</div>
+              )}
+              {errors[formValues.id]?.type === "maxLength" && (
+                <div>{`This field is exceeding the max. character limit`}</div>
+              )}
+            </span>
+          </div>
+        ))}
+      </div>
+      <label className="col-span-2 mt-5 flex h-48 flex-col justify-center rounded-lg border-2 border-dashed border-gray-300 bg-stone-100 text-center lg:hidden">
+        <div className="mb-3 flex items-center justify-center">
+          <input type="file" className="hidden" />
+          <div className="text-base font-medium text-gray-500">
+            {imageTitle}
+          </div>
+        </div>
+        <div className="text-sm text-gray-300">
+          The file size not more than 10 MB.
+        </div>
+        <div className="text-sm text-gray-300">JPEG, PNG, Video</div>
+      </label>
+
+      <div className="mt-10">
+        <div className="mb-2 text-center font-heading text-2xl font-medium uppercase lg:text-left">
+          {tableTitle}
+        </div>
+        <div className="text-center text-gray-400 lg:text-left">
+          {tableDescription}
+        </div>
+        <div className="mt-4 flex flex-col items-start lg:flex-row">
+          <textarea
+            placeholder="Medical Pre-History 1"
+            className="border-1 h-40 w-full grow rounded-lg border-gray-300 pl-5 focus:border-gray-600 focus:outline-none focus:ring-0 lg:h-20"
+            onChange={(e)=>{
+                handleChangeCurrentData("medicalHistory",e.target.value)
+            }}
+          />
+          <Button
+            className="border-1 mx-3 hidden rounded-md border-blush-light px-8 py-3 font-medium text-blush-light hover:border-blush-dark hover:text-blush-dark lg:block"
+            type="button"
+            onClick={()=>addTableData(currentTableData)}
+          >
+            Add
+          </Button>
+          <button className="mt-5 flex items-center lg:hidden">
+            <div className="inline-flex h-10 w-10 items-center justify-center rounded-full bg-black p-3">
+              <Image src={Plus} className="" alt="" />
+            </div>
+            <div className="ml-3">{mobileAddButtonText}</div>
+          </button>
+        </div>
+        <div className="scroll mt-5 hidden max-h-[370px] overflow-auto px-0 lg:block">
+          <table className="common-table w-full min-w-max table-fixed  border-separate border-spacing-y-3 text-left">
+            <thead>
+              <tr>
+                {TableHeadings?.map((head, index) => {
+                  return (
+                    <th
+                      className="w-20 pl-7 font-medium text-gray-400"
+                      key={index}
+                    >
+                      {head.label}
+                    </th>
+                  );
+                })}
+
+              </tr>
+            </thead>
+            <tbody>
+              {tableData?.map((data, dataIndex) => {
+                return (
+                  <tr key={dataIndex}>
+                    {TableHeadings?.map((head, headIndex) => {
+                      return (
+                        <div key={headIndex}>
+                          {head?.id !== "action" ? (
+                            <td className="w-20 border-y-2 border-gray-100 p-4">
+                              {data[head?.id]}
+                            </td>
+                          ) : (
+                            <td className="w-32 border-y-2 border-gray-100 p-4 font-medium text-gray-400">
+                              Remove
+                            </td>
+                          )}
+                        </div>
+                      );
+                    })}
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+      {buttonItems?.next && (
+        <div className="bottom-8 right-0 mr-10 mt-10 flex justify-end lg:absolute">
+          <button
+            className="w-full rounded-full !border-0 bg-mandy-dark px-5 py-3   text-white outline-0 hover:bg-mandy-dark focus:outline-none focus:ring focus:ring-0 lg:w-auto lg:rounded lg:py-1.5"
+            type="button"
+            onClick={nextClickHandler}
+          >
+            Next
+          </button>
+        </div>
+      )}
+
+      {buttonItems?.prev && buttonItems?.finish && (
+        <div className="bottom-8 right-0 mr-10 mt-10 flex justify-end lg:absolute">
+          <Button
+            type="button"
+            className="mx-3 bg-pink-600 hover:bg-pink-800"
+            onClick={prevClickHandler}
+          >
+            Prev
+          </Button>
+          <Button
+            type="button"
+            className="mx-3 bg-pink-600 hover:bg-pink-800"
+            onClick={submitCallback}
+          >
+            Finish
+          </Button>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default AddForm;
--- src/components/AddAthlete/AddAthlete.tsx
+++ src/components/AddAthlete/AddAthlete.tsx
@@ -17,6 +17,7 @@ import { DividerHorizontalIcon } from "@radix-ui/react-icons";
 import Image from "next/image";
 import SearchIcon from "../../images/search.png";
 import Plus from "../../images/plus.svg";
+import AddForm from "~/common/AddForm";
 
 export default function AddCoach() {
   let inputElement;
@@ -24,6 +25,7 @@ export default function AddCoach() {
     stepData: { currentStep, setCurrentStep },
     multiFormData: { formData, setFormData },
   } = useContext(FormContext);
+  const [medicalHistoryData,setMedicalHistoryData]=useState([])
 
   const {
     control,
@@ -64,7 +66,6 @@ export default function AddCoach() {
 
   useEffect(() => {
     // if (!isEditMode) {
-    // eslint-disable-next-line no-console
     reset({
       ...currentFormValues,
       ...formData,
@@ -73,196 +74,18 @@ export default function AddCoach() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [formData]);
   //test commit
-  const getInputElement = (props: COACH_DETAILS_CONSTANTS_TYPES) => {
-    const { type, rules, id, pattern, placeHolder } = props;
-    switch (type) {
-      case "select":
-        const { options } = props;
-        inputElement = (
-          <Controller
-            control={control}
-            name={id}
-            rules={rules}
-            render={({ field: { onChange, value } }) => {
-              return (
-                <Select
-                  isMulti={props?.isMulti ?? false}
-                  options={options}
-                  value={value}
-                  placeholder={placeHolder}
-                  className="w-full border-1 border-gray-300 c-select"
-                  classNamePrefix="react-select"
-                  onChange={(element) => {
-                    onChange(element);
-                  }}
-                />
-              );
-            }}
-          />
-        );
-        break;
-      case "calendar":
-        inputElement = (
-          <Controller
-            control={control}
-            render={({ field: { onChange, value } }) => {
-              return (
-                <Datepicker
-                  placeHolder={props.placeHolder}
-                  value={new Date(value as string)}
-                  className="h-12"
-                  onChangeHandler={onChange}
-                />
-              );
-            }}
-            name={id}
-            rules={rules}
-          />
-        );
-        break;
-      default:
-        inputElement = (
-          <Controller
-            control={control}
-            name={id}
-            render={({ field: { onChange, value } }) => (
-              <Textbox
-                className="h-12 w-full"
-                placeHolder={props.label}
-                onChangeHandler={onChange}
-                // TODO: FIX THIS TS ERROR
-                value={value as string}
-              />
-            )}
-            rules={rules}
-            {...(pattern ? { pattern } : {})}
-          />
-        );
-    }
 
-    return inputElement;
-  };
 
-  const nextClickHandler = async () => {
-    const result = await trigger();
-    if (result) {
-      const currentFormValues = getValues();
-      setFormData && setFormData({ ...formData, ...currentFormValues });
-      setCurrentStep && setCurrentStep(currentStep + 1);
-    }
-  };
+  const addTableData=(currentData)=>{
+    let arr=[...medicalHistoryData]
+    let obj={...currentData,["No."]:arr.length+1}
+    arr.push(obj)
+    setMedicalHistoryData(arr)
+  }
 
   return (
     <>
-
-      <CardTitle title="ADD ATHLETE" />
-      <div className=" font-medium uppercase text-3xl font-heading text-center lg:text-left" >Athlete Personal Details</div>
-      <div className="mt-8 grid lg:grid-cols-2 grid-col-1 gap-x-8 lg:gap-y-8 gap-y-4 ">
-        {formConstantValues.map((props) => (
-          <div key={props.id}>
-            {getInputElement(props)}
-
-            <span className="text-red-800">
-              {errors[props.id]?.type === "required" && (
-                <div>This field is required</div>
-              )}
-              {errors[props.id]?.type === "pattern" && (
-                <div> This field is not matching the pattern</div>
-              )}
-              {errors[props.id]?.type === "maxLength" && (
-                <div>{`This field is exceeding the max. character limit`}</div>
-              )}
-            </span>
-          </div>
-        ))}
-      </div>
-      <label className="lg:hidden col-span-2 bg-stone-100 rounded-lg border-2 border-dashed border-gray-300 mt-5 text-center h-48 flex justify-center flex-col">
-        <div className="flex justify-center items-center mb-3">
-          <input type="file" className="hidden" />
-          <div className="font-medium text-base text-gray-500">Attach Athlete Image</div>
-        </div>
-        <div className="text-gray-300 text-sm">The file size not more than 10 MB.</div>
-        <div className="text-gray-300 text-sm">JPEG, PNG, Video</div>
-      </label>
-
-
-      <div className="mt-10">
-        <div className="text-2xl mb-2 font-medium font-heading uppercase text-center lg:text-left">Medical History</div>
-        <div className="text-gray-400 text-center lg:text-left">Kindly list down if your child has any allergies, major injuries, chronic diseases, physical disabilities &
-          Children with special needs (CWSN)</div>
-        <div className="flex mt-4 items-start lg:flex-row flex-col">
-          <textarea placeholder="Medical Pre-History 1" className="w-full grow lg:h-20 h-40 rounded-lg border-1 border-gray-300 pl-5 focus:outline-none focus:ring-0 focus:border-gray-600" />
-          <Button
-            className="lg:block hidden border-1 mx-3 px-8 py-3 font-medium border-blush-light hover:border-blush-dark rounded-md text-blush-light hover:text-blush-dark"
-            type="button"
-          >
-            Add
-          </Button>
-          <button className="mt-5 lg:hidden flex items-center">
-            <div className="bg-black rounded-full p-3 w-10 h-10 inline-flex justify-center items-center">
-              <Image src={Plus} className="" alt="" />
-            </div>
-            <div className="ml-3">Add another medical history</div>
-          </button>
-
-
-        </div>
-        <div className="mt-5 max-h-[370px] overflow-auto px-0 scroll lg:block hidden">
-          <table className="table-fixed common-table w-full min-w-max  text-left border-separate border-spacing-y-3">
-            <thead>
-              <tr>
-                <th className="pl-7 w-20 text-gray-400 font-medium" >#</th>
-                <th className="text-justify text-gray-400 font-medium">Medical History </th>
-                <th className="w-32 text-gray-400 font-medium">Action</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td className="p-4 border-y-2 border-gray-100 w-20" >01</td>
-                <td className="p-4 border-y-2 border-gray-100  text-justify">
-                  <div>
-                    Injuries: quo possimus dolores nam autem ipsa. Est soluta quia et blanditiis sunt qui asperiores tempore eos similique veniam aut maxime veniam aut possimus possimus qui voluptatem maiores.AAA
-                  </div>
-                </td>
-                <td className="p-4 border-y-2 border-gray-100 w-32 text-gray-400 font-medium">
-                  Remove
-                </td>
-              </tr>
-              <tr>
-                <td className="p-4 border-y-2 border-gray-100 w-20" >01</td>
-                <td className="p-4 border-y-2 border-gray-100  text-justify">
-                  <div>
-                    Injuries: quo possimus dolores nam autem ipsa. Est soluta quia et blanditiis sunt qui asperiores tempore eos similique veniam aut maxime veniam aut possimus possimus qui voluptatem maiores.AAA
-                  </div>
-                </td>
-                <td className="p-4 border-y-2 border-gray-100 w-32 text-gray-400 font-medium">
-                  Remove
-                </td>
-              </tr>
-              <tr>
-                <td className="p-4 border-y-2 border-gray-100 w-20" >01</td>
-                <td className="p-4 border-y-2 border-gray-100  text-justify">
-                  <div>
-                    Injuries: quo possimus dolores nam autem ipsa. Est soluta quia et blanditiis sunt qui asperiores tempore eos similique veniam aut maxime veniam aut possimus possimus qui voluptatem maiores.AAA
-                  </div>
-                </td>
-                <td className="p-4 border-y-2 border-gray-100 w-32 text-gray-400 font-medium">
-                  Remove
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </div>
-      <div className="mr-10 mt-10 flex justify-end lg:absolute bottom-8 right-0">
-        <button
-          className="!border-0 px-5 py-3 lg:py-1.5 lg:rounded rounded-full   focus:ring-0 outline-0 bg-mandy-dark hover:bg-mandy-dark focus:outline-none focus:ring text-white w-full lg:w-auto"
-          type="button"
-          onClick={() => void nextClickHandler()}
-        >
-          Next
-        </button>
-      </div>
+    <AddForm cardTitle="ADD ATHLETE" cardSubTitle="Athlete Personal Details" formConstantValues={formConstantValues} imageTitle="Attach Athlete Image" tableTitle="Medical History" tableDescription={"Kindly list down if your child has any allergies, major injuries, chronic diseases, physical disabilities & Children with special needs (CWSN)"} mobileAddButtonText="Add another medical history" TableHeadings={[{label:"#",id:"No."},{label:"Medical History",id:"medicalHistory"},{label:"Action",id:"action"}]} tablekey="medicalHistory" tableData={medicalHistoryData} addTableData={addTableData} buttonItems={{next:true}} setFormData={setFormData} formData={formData} currentStep={currentStep} setCurrentStep={setCurrentStep}/>
 
     </>
   )
