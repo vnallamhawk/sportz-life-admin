685ed34476db253f573aeb0eda5787454e64796c
Rakesh Nallam <rakeshnallam@Rakeshs-MBP-2.lan>
2025-03-15 15:48:24 -0700

    disable any rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -23,6 +23,7 @@ const config = {
   rules: {
     '@typescript-eslint/consistent-type-imports': 'error',
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/no-explicit-any": "off",
     "no-console": ["error", { allow: ["error"] }],
   },
 };
