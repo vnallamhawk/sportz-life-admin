a595a89f53ed24a5d7bbd5ac03ad1046f96f299e
Rakesh Nallam <rakeshnallam@Rakeshs-MBP-2.lan>
2025-03-16 20:14:39 -0700

    chore: comment unused files

--- src/components/AllCoaches/CoachTableBody.tsx
+++ src/components/AllCoaches/CoachTableBody.tsx
@@ -1,105 +1,105 @@
-import * as Checkbox from "@radix-ui/react-checkbox";
-import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
-import { HamburgerMenuIcon, CheckIcon } from "@radix-ui/react-icons";
-import { api } from "~/utils/api";
-import { differenceInYears } from "date-fns";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
-import { NO_DATA } from "~/globals/globals";
-import type { Coaches, Sports } from "@prisma/client";
-import type { CoachSportsMaps } from "@prisma/client";
+// import * as Checkbox from "@radix-ui/react-checkbox";
+// import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
+// import { HamburgerMenuIcon, CheckIcon } from "@radix-ui/react-icons";
+// import { api } from "~/utils/api";
+// import { differenceInYears } from "date-fns";
+// import { useRouter } from "next/router";
+// import { useEffect } from "react";
+// import { NO_DATA } from "~/globals/globals";
+// import type { Coaches, Sports } from "@prisma/client";
+// import type { CoachSportsMaps } from "@prisma/client";
 
-interface coachTableFilter {
-  name: string;
-}
+// interface coachTableFilter {
+//   name: string;
+// }
 
-type Coach = Coaches & {
-  CoachSportsMaps?: CoachSportsMaps[];
-  sports?:string
-};
+// type Coach = Coaches & {
+//   CoachSportsMaps?: CoachSportsMaps[];
+//   sports?:string
+// };
 
-export default function CoachTableBody(
-  filter: coachTableFilter,
-  handleIsLoading: (isLoading: boolean) => void
-) {
-  let tableData:Coach[]=[]
-  const router = useRouter();
+// export default function CoachTableBody(
+//   filter: coachTableFilter,
+//   handleIsLoading: (isLoading: boolean) => void
+// ) {
+//   let tableData:Coach[]=[]
+//   const router = useRouter();
 
-  const { data: coaches } =
-    filter.name == ""
-      ? api.coach.getAllCoaches.useQuery()
-      : api.coach.getCoachesByName.useQuery(filter);
-  const { data: sports, isLoading } = api.sports.getAllSports.useQuery();
+//   const { data: coaches } =
+//     filter.name == ""
+//       ? api.coach.getAllCoaches.useQuery()
+//       : api.coach.getCoachesByName.useQuery(filter);
+//   const { data: sports, isLoading } = api.sports.getAllSports.useQuery();
 
-  if (coaches && sports) {
-    tableData=
-      coaches && coaches.length>0 ?
-       coaches.map((coach: Coach) => {
-        return {
-          ...coach,
-          sports: coach?.CoachSportsMaps?.length
-            ? coach?.CoachSportsMaps?.map(
-                (sport: CoachSportsMaps) => sports.find((s:Sports) => s.id === sport.sportId)?.name
-              )?.join(",")
-            : "",
-        };
-      }):[];
-  }
+//   if (coaches && sports) {
+//     tableData=
+//       coaches && coaches.length>0 ?
+//        coaches.map((coach: Coach) => {
+//         return {
+//           ...coach,
+//           sports: coach?.CoachSportsMaps?.length
+//             ? coach?.CoachSportsMaps?.map(
+//                 (sport: CoachSportsMaps) => sports.find((s:Sports) => s.id === sport.sportId)?.name
+//               )?.join(",")
+//             : "",
+//         };
+//       }):[];
+//   }
 
-  useEffect(() => {
-    handleIsLoading(isLoading);
-  }, [handleIsLoading, isLoading]);
+//   useEffect(() => {
+//     handleIsLoading(isLoading);
+//   }, [handleIsLoading, isLoading]);
 
-  const onClickHandler = (id: number) => {
-    void router.push(`/coach/${id ?? ""}`);
-  };
+//   const onClickHandler = (id: number) => {
+//     void router.push(`/coach/${id ?? ""}`);
+//   };
 
-  return (
-    <>
-      {tableData?.map((data,index) => (
-          <tr
-            key={`${data?.name}-${index}`}
-            className="cursor-pointer border-b border-gray-200 hover:bg-gray-100"
-            onClick={() => onClickHandler(data?.id)}
-          >
-            <td className="rounded-l-lg border-y-2 border-l-2 border-solid pl-5">
-              <Checkbox.Root className="CheckboxRoot" defaultChecked id="c1">
-                <Checkbox.Indicator className="CheckboxIndicator">
-                  <CheckIcon />
-                </Checkbox.Indicator>
-              </Checkbox.Root>
-            </td>
-            <td className="whitespace-nowrap border-y-2 border-solid px-6 py-3 text-left">
-              {data?.name}
-            </td>
-            <td className="border-y-2 border-solid px-6 py-3 text-left">
-              {differenceInYears(new Date(), new Date(data?.dateOfBirth))}
-            </td>
-            <td className="border-y-2 border-solid px-6 py-3 text-left">
-              {data?.designation}
-            </td>
-            <td className="border-y-2 border-solid px-6 py-3 text-left">
-              {data.sports ? data.sports : NO_DATA}
-            </td>
-            <td className="border-y-2 border-solid px-6 py-3 text-left">
-              {data?.gender}
-            </td>
-            <td className="border-y-2 border-solid px-6 py-3 text-left">{`batch`}</td>
-            <td className="border-y-2 border-solid px-6 py-3 text-left">
-              {`${data?.countryCode}${data?.phone as string}`}
-            </td>
-            <td className="rounded-r-lg border-y-2 border-r-2 border-solid px-6 py-3 text-left">
-              <DropdownMenu.Root>
-                <DropdownMenu.Trigger asChild>
-                  <button className="IconButton" aria-label="Customise options">
-                    <HamburgerMenuIcon />
-                  </button>
-                </DropdownMenu.Trigger>
-              </DropdownMenu.Root>
-            </td>
-          </tr>
-        )
-      )}
-    </>
-  );
-}
+//   return (
+//     <>
+//       {tableData?.map((data,index) => (
+//           <tr
+//             key={`${data?.name}-${index}`}
+//             className="cursor-pointer border-b border-gray-200 hover:bg-gray-100"
+//             onClick={() => onClickHandler(data?.id)}
+//           >
+//             <td className="rounded-l-lg border-y-2 border-l-2 border-solid pl-5">
+//               <Checkbox.Root className="CheckboxRoot" defaultChecked id="c1">
+//                 <Checkbox.Indicator className="CheckboxIndicator">
+//                   <CheckIcon />
+//                 </Checkbox.Indicator>
+//               </Checkbox.Root>
+//             </td>
+//             <td className="whitespace-nowrap border-y-2 border-solid px-6 py-3 text-left">
+//               {data?.name}
+//             </td>
+//             <td className="border-y-2 border-solid px-6 py-3 text-left">
+//               {differenceInYears(new Date(), new Date(data?.dateOfBirth))}
+//             </td>
+//             <td className="border-y-2 border-solid px-6 py-3 text-left">
+//               {data?.designation}
+//             </td>
+//             <td className="border-y-2 border-solid px-6 py-3 text-left">
+//               {data.sports ? data.sports : NO_DATA}
+//             </td>
+//             <td className="border-y-2 border-solid px-6 py-3 text-left">
+//               {data?.gender}
+//             </td>
+//             <td className="border-y-2 border-solid px-6 py-3 text-left">{`batch`}</td>
+//             <td className="border-y-2 border-solid px-6 py-3 text-left">
+//               {`${data?.countryCode}${data?.phone as string}`}
+//             </td>
+//             <td className="rounded-r-lg border-y-2 border-r-2 border-solid px-6 py-3 text-left">
+//               <DropdownMenu.Root>
+//                 <DropdownMenu.Trigger asChild>
+//                   <button className="IconButton" aria-label="Customise options">
+//                     <HamburgerMenuIcon />
+//                   </button>
+//                 </DropdownMenu.Trigger>
+//               </DropdownMenu.Root>
+//             </td>
+//           </tr>
+//         )
+//       )}
+//     </>
+//   );
+// }
--- src/pages/trainingPlan/TrainingPlan.tsx
+++ src/pages/trainingPlan/TrainingPlan.tsx
@@ -5,23 +5,24 @@ import Textbox from "~/components/Textbox/Textbox";
 import Button from "~/components/Button/Button";
 import Table from "~/components/Table/Table";
 import CoachTableHeader from "../../components/AllCoaches/CoachTableHeader";
-import CoachTableBody from "../../components/AllCoaches/CoachTableBody";
+// import CoachTableBody from "../../components/AllCoaches/CoachTableBody";
 import { useRouter } from "next/navigation";
 import LoadingSpinner from "~/components/LoadingSpinner/LoadingSpinner";
 import TabsComponent from "~/components/Tabs";
 import MultiTabComp from "~/common/MultiTabComp";
-import { INJURY_ATHLETE_TABLE_HEADER, INJURY_COACH_TABLE_HEADER } from "~/constants/injuryLog";
+import {
+  INJURY_ATHLETE_TABLE_HEADER,
+  INJURY_COACH_TABLE_HEADER,
+} from "~/constants/injuryLog";
 
 export default function AllTrainingPlans() {
   const router = useRouter();
 
-  const [activeKey,setActiveKey]=useState("0")
+  const [activeKey, setActiveKey] = useState("0");
 
   const [filterByName, setFilterByName] = useState("");
   const [loading, setLoading] = useState(true);
   const [filters, setFilters] = useState<{ [key: string]: any }>([]);
-
-
 
   const handleFilters = (appliedFilters: { [key: string]: any }) => {
     setFilters(appliedFilters);
@@ -31,16 +32,18 @@ export default function AllTrainingPlans() {
   };
 
   return (
-   <>
-     {/* <Checkout /> */}
-     <MultiTabComp
+    <>
+      {/* <Checkout /> */}
+      <MultiTabComp
         tab1label="FITNESS PLAN"
         tab2label="COACHING PLAN"
         addButtonText="Add Plan"
-        setActiveKey={(key:string)=>setActiveKey(key)}              
+        setActiveKey={(key: string) => setActiveKey(key)}
         activeKey={activeKey}
         addButtonUrl={
-          activeKey==="0"?"/trainingPlan/AddTrainingPlan/FitnessPlan":"/trainingPlan/AddTrainingPlan/CoachingPlan"
+          activeKey === "0"
+            ? "/trainingPlan/AddTrainingPlan/FitnessPlan"
+            : "/trainingPlan/AddTrainingPlan/CoachingPlan"
         }
         filters={[]}
         applyFilters={(appliedFilters: { [key: string]: any }) =>
@@ -60,6 +63,6 @@ export default function AllTrainingPlans() {
         // onEditClick={(id: number) => {}}
         // onDeleteClick={(id: number) => {}}
       />
-   </>
+    </>
   );
 }
