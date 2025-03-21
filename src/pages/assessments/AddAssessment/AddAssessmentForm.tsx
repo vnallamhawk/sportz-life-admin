edd899712eb093e6b2b93d3bd1d7705f60fdcc75
Rakesh Nallam <rakeshnallam@Rakeshs-MBP-2.lan>
2025-03-21 10:17:24 -0700

    fix: add assessment

--- src/pages/assessments/AddAssessment/AddAssessmentForm.tsx
+++ src/pages/assessments/AddAssessment/AddAssessmentForm.tsx
@@ -52,44 +52,47 @@ export default function AddAssessmentForm() {
   const router = useRouter();
   const id = Number(router?.query?.id);
   const { data: sessionData } = useSession();
-  const  createdBy= sessionData?.token?sessionData?.token?.id:sessionData?.user?.id
-  const  academyId= sessionData?.token?sessionData?.token?.academyId:sessionData?.user?.academyId
+  const createdBy = sessionData?.token
+    ? sessionData?.token?.id
+    : sessionData?.user?.id;
+  const academyId = sessionData?.token
+    ? sessionData?.token?.academyId
+    : sessionData?.user?.academyId;
 
   const methods = useForm();
   const [currentStep, setCurrentStep] = useState<number>(1);
   const [formData, setFormData] = useState<any>(
     defaultValues.multiFormData.formData
   );
-  const [center,setCenter]=useState<Centers>()
+  const [center, setCenter] = useState<Centers>();
   const [centerId, setCenterId] = useState<number>();
 
   const { setOpenToast } = useContext(ToastContext);
   const [preview, setPreview] = useState<(File & { preview: string })[]>([]);
-  const centerData=  id && api.center.getCenterById.useQuery({ id });
+  const centerData = id && api.center.getCenterById.useQuery({ id });
 
   //   const { data: batches } = api.batches.getAllBatches.useQuery();
   const hasCenterUseEffectRun = useRef(false);
 
-
   useEffect(() => {
-    if (centerData &&centerData.data ) {
-
+    if (centerData && centerData.data) {
       if (centerData.data && !hasCenterUseEffectRun.current) {
-        let obj:any={...centerData.data}
-        obj.sports=centerData?.data?.CenterSports?.map((v:any) => ({
-          sportId:v.sportId,
-          id:v.id
+        let obj: any = { ...centerData.data };
+        obj.sports = centerData?.data?.CenterSports?.map((v: any) => ({
+          sportId: v.sportId,
+          id: v.id,
         }));
-        obj.inventories=centerData?.data?.CenterInventories?.map((v:any) => ({
-          inventoryId:v.inventoryId,
-          id:v.id
-        }));
+        obj.inventories = centerData?.data?.CenterInventories?.map(
+          (v: any) => ({
+            inventoryId: v.inventoryId,
+            id: v.id,
+          })
+        );
         setFormData(obj);
         hasCenterUseEffectRun.current = true;
       }
     }
   }, [centerData]);
-
 
   const formProviderData = {
     ...methods,
@@ -146,15 +149,15 @@ export default function AddAssessmentForm() {
       formData?.inventories &&
       centerId
     ) {
-      const finalCenterSports = formData?.sports?.map((v:any) => ({
+      const finalCenterSports = formData?.sports?.map((v: any) => ({
         ...v,
         centerId,
-        academyId:sessionData?.token?.academyId
+        academyId: sessionData?.token?.academyId,
       }));
 
       createMutateCenterSports(finalCenterSports);
 
-      const finalInventories = formData?.inventories?.map((v:any) => ({
+      const finalInventories = formData?.inventories?.map((v: any) => ({
         ...v,
         centerId,
       }));
@@ -162,24 +165,20 @@ export default function AddAssessmentForm() {
     }
   }, [centerId, formData]);
 
-  const finalFormSubmissionHandler = async (
-    finalForm:any
-  ) => {
+  const finalFormSubmissionHandler = async (finalForm: any) => {
     if (formData?.isEditMode) {
       editMutate({
         ...finalForm,
         mobile: finalForm?.mobile,
         address: finalForm?.address,
         image: "",
-        centerId:id,
-        updatedAt:new Date()
+        centerId: id,
+        updatedAt: new Date(),
       });
     } else {
-      // eslint-disable-next-line no-console
-      console.log(finalForm);
-      // eslint-disable-next-line no-console
-      console.log(finalForm, "djbsdbfn");
-      const sportsId = finalForm?.selectSports?.map(function (obj: { value: any; }) {
+      const sportsId = finalForm?.selectSports?.map(function (obj: {
+        value: any;
+      }) {
         return Number(obj.value);
       });
       setFormData({
@@ -195,9 +194,9 @@ export default function AddAssessmentForm() {
         address: finalForm?.address,
         image: "",
         createdBy: parseInt(createdBy as string),
-        academyId:parseInt(academyId as string),
-        createdAt:new Date(),
-        updatedAt:new Date()
+        academyId: parseInt(academyId as string),
+        createdAt: new Date(),
+        updatedAt: new Date(),
       });
     }
   };
@@ -209,7 +208,11 @@ export default function AddAssessmentForm() {
             {currentStep === 1 && <AddAssessment />}
             {currentStep === 2 && <AssignTestBank />}
             {currentStep === 3 && <AddAssessmentScoring />}
-            {currentStep === 4 && <AddAssessmentSchedule  finalFormSubmissionHandler={finalFormSubmissionHandler}/>}
+            {currentStep === 4 && (
+              <AddAssessmentSchedule
+                finalFormSubmissionHandler={finalFormSubmissionHandler}
+              />
+            )}
 
             {/* {currentStep === 3 && (
               <AddInventory
@@ -217,7 +220,6 @@ export default function AddAssessmentForm() {
               />
             )}  */}
           </Card>
-
         </div>
       </div>
     </FormContext.Provider>
