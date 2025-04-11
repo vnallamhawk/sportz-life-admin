6d14a9310988894eda60dfcb841fcb2b9466c8ce
Nivash Sudalaimani <154258573+nivashs@users.noreply.github.com>
2025-04-11 21:35:28 +0530

    fixed edit,view athlete not working

--- src/pages/athlete/AddAthlete/AddAthleteMultiFormLayout.tsx
+++ src/pages/athlete/AddAthlete/AddAthleteMultiFormLayout.tsx
@@ -1,47 +1,48 @@
 /* eslint-disable */
-import React, {
-  useState,
-  useContext,
-  useCallback,
-  useEffect,
-  useRef,
-} from "react";
-import Card from "~/components/Card";
-import ImageWithFallback from "~/components/ImageWithFallback";
-import { useForm } from "react-hook-form";
+import React, {useState, useContext, useCallback, useEffect, useRef} from 'react'
+import Card from '~/components/Card'
+import ImageWithFallback from '~/components/ImageWithFallback'
+import {useForm, FormProvider} from 'react-hook-form'
 import {
   type TRAINING_LEVEL,
   type GENDER_VALUES,
   type MULTI_FORM_TYPES,
   type EXPERIENCE_LEVEL,
-} from "~/types/coach";
-import { api } from "~/utils/api";
-import { useRouter } from "next/router";
-import { ToastContext } from "~/contexts/Contexts";
-import FileUpload from "~/components/FileUpload";
-import { getSportsDictionaryServices } from "~/services/sportServices";
+} from '~/types/coach'
+import {api} from '~/utils/api'
+import {useRouter} from 'next/router'
+import {ToastContext} from '~/contexts/Contexts'
+import FileUpload from '~/components/FileUpload'
+import {getSportsDictionaryServices} from '~/services/sportServices'
 
-import AddAthlete from "../../../components/AddAthlete/AddAthlete";
-import DashboardHeader from "~/components/DashboardHeader";
-import AddGeneralDetails from "~/components/AddAthlete/AddGeneralDetails";
-import { useSession } from "next-auth/react";
+import AddAthlete from '../../../components/AddAthlete/AddAthlete'
+import DashboardHeader from '~/components/DashboardHeader'
+import AddGeneralDetails from '~/components/AddAthlete/AddGeneralDetails'
+import {useSession} from 'next-auth/react'
+import type {ATHLETE_TYPES} from '~/types/athlete'
 
 // const multiFormData: MULTI_FORM_TYPES = {
 const multiFormData = {
-  phone: "",
-  name: "",
-  bloodGroup: "",
-  email: "",
-  about: "",
+  phone: '',
+  name: '',
+  bloodGroup: '',
+  email: '',
+  about: '',
   dob: undefined,
-  payroll: "",
+  payroll: '',
   coachingSports: [],
   certificates: [],
   batchIds: [],
   centerId: undefined,
   isEditMode: false,
   coachId: undefined,
-};
+  height: '',
+  weight: '',
+  address: '',
+  medicalHistory: [],
+  fatherName: '',
+  athleteId: undefined,
+}
 
 const defaultValues = {
   stepData: {
@@ -50,95 +51,88 @@ const defaultValues = {
   multiFormData: {
     formData: multiFormData,
   },
-};
+}
+
 export interface FormContextTypes {
   stepData: {
-    currentStep: number;
-    setCurrentStep?: React.Dispatch<React.SetStateAction<number>>;
-  };
+    currentStep: number
+    setCurrentStep?: React.Dispatch<React.SetStateAction<number>>
+  }
   multiFormData: {
-    formData: any;
-    setFormData?: React.Dispatch<React.SetStateAction<any>>;
-  };
+    formData: any
+    setFormData?: React.Dispatch<React.SetStateAction<any>>
+  }
 }
-export const FormContext = React.createContext<FormContextTypes>(defaultValues);
+
+export const FormContext = React.createContext<FormContextTypes>(defaultValues)
 
 export default function AddAthleteMultiFormLayout() {
-  const router = useRouter();
-  const id = Number(router?.query?.id);
-  const hasRun = useRef(false); // Track if useEffect has already run
+  const router = useRouter()
+  const id = Number(router?.query?.id)
+  const hasRun = useRef(false)
 
-  const methods = useForm();
-  const [currentStep, setCurrentStep] = useState<number>(1);
-  const [formData, setFormData] = useState<any>(
-    defaultValues.multiFormData.formData
-  );
-  const { data: sessionData } = useSession();
-  const [athleteId, setAthleteId] = useState<number>();
+  const methods = useForm<ATHLETE_TYPES>({mode: 'onSubmit'})
+  const [currentStep, setCurrentStep] = useState<number>(1)
+  const [formData, setFormData] = useState<any>(defaultValues.multiFormData.formData)
+  const {data: sessionData} = useSession()
+  const [athleteId, setAthleteId] = useState<number>()
+  const [preview, setPreview] = useState<(File & {preview: string})[]>([])
+  const [file, setFile] = useState<File | null>(null)
+  const [uploadUrl, setUploadUrl] = useState<string>('')
+  const uploadImage = api.upload.uploadImage.useMutation()
+  const athleteData = id && api.athlete.getAthleteById.useQuery({id})
+  const hasUseEffectRun = useRef(false)
 
-  const { setOpenToast } = useContext(ToastContext);
-  const [preview, setPreview] = useState<(File & { preview: string })[]>([]);
+  const {setOpenToast} = useContext(ToastContext)
   const createdBy = sessionData?.token ? sessionData?.token?.id : sessionData?.user?.id
-  const academyId = sessionData?.token ? sessionData?.token?.academyId : sessionData?.user?.academyId
-  const [file, setFile] = useState<File | null>(null);
-  const [uploadUrl, setUploadUrl] = useState<string>("");
-  const uploadImage = api.upload.uploadImage.useMutation();
-  const athleteData = id && api.athlete.getAthleteById.useQuery({ id });
-  const hasUseEffectRun = useRef(false);
-
+  const academyId = sessionData?.token
+    ? sessionData?.token?.academyId
+    : sessionData?.user?.academyId
 
   useEffect(() => {
-    if (athleteData && athleteData.data && !hasUseEffectRun.current) {
-      let obj: any = { ...athleteData.data };
-
-      obj.isEditMode = true;
-      setFormData(obj);
-      hasUseEffectRun.current = true;
+    if (athleteData?.data && !hasUseEffectRun.current) {
+      const obj = {...athleteData.data}
+      obj.isEditMode = true
+      setFormData(obj)
+      hasUseEffectRun.current = true
     }
-  }, [athleteData]);
-
+  }, [athleteData?.data])
 
   const formProviderData = {
-    ...methods,
-    stepData: { currentStep, setCurrentStep },
-    multiFormData: { formData, setFormData },
-  };
-  const { mutate: createMutate } = api.athlete.createAthlete.useMutation({
-    onSuccess: (response) => {
-      console.log("response data is ", response);
-      setOpenToast(true);
-      setAthleteId(response?.id)
+    stepData: {currentStep, setCurrentStep},
+    multiFormData: {formData, setFormData},
+  }
 
-      return response
-    },
-  });
-
-  const { mutate: editMutate } = api.athlete.editAthlete.useMutation({
+  const {mutate: createMutate} = api.athlete.createAthlete.useMutation({
     onSuccess: (response) => {
-      console.log("response data is ", response);
-      setOpenToast(true);
+      setOpenToast(true)
       setAthleteId(response?.id)
       return response
     },
-  });
+  })
 
-  const { mutate: createMutateAthleteSports } =
-    api.athleteSports.createAthleteSports.useMutation({
-      onSuccess: (response) => {
-        console.log("response data in athelete sports ", response);
+  const {mutate: editMutate} = api.athlete.editAthlete.useMutation({
+    onSuccess: (response) => {
+      setOpenToast(true)
+      setAthleteId(response?.id)
+      void router.push(`/athlete/${response?.id}`).then(() => window.location.reload())
+    },
+  })
 
-        return response;
-      },
-    });
-  const { mutate: createMutateAthleteBatches } =
-    api.athleteBatches.createAthletebatches.useMutation({
-      onSuccess: (response) => {
-        console.log("response data in athelete batches ", response);
-        router.push("/athlete").then(() => window.location.reload());
+  const {mutate: createMutateAthleteSports} = api.athleteSports.createAthleteSports.useMutation({
+    onSuccess: (response) => {
+      console.log('response data in athelete sports ', response)
+      return response
+    },
+  })
 
-        return response;
-      },
-    });
+  const {mutate: createMutateAthleteBatches} = api.athleteBatches.createAthletebatches.useMutation({
+    onSuccess: (response) => {
+      console.log('response data in athelete batches ', response)
+      router.push('/athlete').then(() => window.location.reload())
+      return response
+    },
+  })
 
   const onDropCallback = useCallback((acceptedFiles: Array<File>) => {
     if (acceptedFiles && acceptedFiles.length > 0) {
@@ -148,46 +142,42 @@ export default function AddAthleteMultiFormLayout() {
             preview: URL.createObjectURL(upFile),
           })
         )
-      );
-      const uploadedFile: File | null = acceptedFiles[0]
-        ? acceptedFiles[0]
-        : null;
-      setFile(uploadedFile);
+      )
+      const uploadedFile: File | null = acceptedFiles[0] ? acceptedFiles[0] : null
+      setFile(uploadedFile)
       if (!uploadedFile) {
-        alert('Please select a valid file');
-        return;
+        alert('Please select a valid file')
+        return
       } else {
-        const fileReader = new FileReader();
+        const fileReader = new FileReader()
         fileReader.onloadend = async () => {
-          const base64String = fileReader.result as string;
-
+          const base64String = fileReader.result as string
 
           try {
             const response = await uploadImage.mutateAsync({
               file: base64String,
               filename: uploadedFile.name,
               mimetype: uploadedFile.type,
-            });
-            setUploadUrl(response.url);
+            })
+            setUploadUrl(response.url)
           } catch (err) {
-            console.error("Upload failed:", err);
+            console.error('Upload failed:', err)
           }
-        };
+        }
         fileReader.readAsDataURL(uploadedFile)
       }
     }
-  }, []);
+  }, [])
 
   useEffect(() => {
     if (
-      !hasRun.current && // Ensure it runs only once
+      !hasRun.current &&
       formData &&
       Object.keys(formData)?.length > 0 &&
       formData?.sportId &&
       formData?.batch &&
       athleteId
     ) {
-
       const finalCoachSports = [
         {
           sportsId: parseInt(formData.sportId),
@@ -198,7 +188,7 @@ export default function AddAthleteMultiFormLayout() {
           createdAt: new Date(),
           updatedAt: new Date(),
         },
-      ];
+      ]
 
       const finalAtheleteBatches = [
         {
@@ -210,18 +200,16 @@ export default function AddAthleteMultiFormLayout() {
           createdAt: new Date(),
           updatedAt: new Date(),
         },
-      ];
+      ]
 
-      createMutateAthleteSports(finalCoachSports);
-      createMutateAthleteBatches(finalAtheleteBatches);
+      createMutateAthleteSports(finalCoachSports)
+      createMutateAthleteBatches(finalAtheleteBatches)
 
-      hasRun.current = true; // Prevent further executions
+      hasRun.current = true
     }
-  }, [athleteId, JSON.stringify(formData)]);
+  }, [athleteId, JSON.stringify(formData)])
 
-  const finalFormSubmissionHandler = (
-    finalForm: any
-  ) => {
+  const finalFormSubmissionHandler = (finalForm: any) => {
     if (academyId) {
       if (formData.isEditMode) {
         editMutate({
@@ -235,105 +223,90 @@ export default function AddAthleteMultiFormLayout() {
           weight: parseInt(finalForm.weight),
           address: finalForm.address,
           medicalHistory: finalForm.medicalHistory,
-          // centerId: parseInt(finalForm.centerId.value),
           fatherName: finalForm.fatherName,
-          heightUnit: "cm",
-          weightUnit: "kg",
-          image: uploadUrl,
+          heightUnit: 'cm',
+          weightUnit: 'kg',
+          image: uploadUrl || formData.image,
           updatedAt: new Date(),
-          athleteId: id
-        });
+          athleteId: id,
+        })
       } else {
-        // eslint-disable-next-line no-console
         createMutate({
           name: finalForm.name,
           phone: finalForm.phone,
           email: finalForm.email,
-          bloodGroup: finalForm.bloodGroup,
-          gender: finalForm.gender,
+          bloodGroup: finalForm.bloodGroup.value,
+          gender: finalForm.gender.value as (typeof GENDER_VALUES)[number],
           dob: new Date(finalForm.dob),
           height: parseInt(finalForm.height),
           weight: parseInt(finalForm.weight),
           address: finalForm.address,
           medicalHistory: finalForm.medicalHistory,
-          // centerId: parseInt(finalForm.centerId.value),
           fatherName: finalForm.fatherName,
-          heightUnit: "cm",
-          weightUnit: "kg",
+          heightUnit: 'cm',
+          weightUnit: 'kg',
           image: uploadUrl,
           createdAt: new Date(),
           updatedAt: new Date(),
-          academyCode: parseInt(academyId as string)
-
-        });
+          academyCode: parseInt(academyId as string),
+        })
       }
     }
-
-  };
+  }
 
   return (
-    <div className="bg-s-gray px-6 pb-7">
+    <div className='bg-s-gray px-6 pb-7'>
       <FormContext.Provider value={formProviderData}>
-        <div className="relative grid grid-cols-6 grid-rows-1">
-          <Card className="relative col-span-12 h-full !rounded-r-none rounded-l-xl bg-white p-0 pt-10 lg:col-span-4">
-            {currentStep === 1 && <AddAthlete />}
-            {currentStep === 2 && (
-              <AddGeneralDetails
-                finalFormSubmissionHandler={finalFormSubmissionHandler}
-              />
-            )}
-          </Card>
-          <Card className="col-span-2 hidden !rounded-l-none rounded-r-xl bg-stone-100 px-7 lg:block">
-            <div className="mb-10 font-heading text-2xl font-medium uppercase">
-              Athlete Image
-            </div>
-
-            <div>
-              {preview.length ? (
-                preview.map((upFile, index) => {
-                  return (
-                    <div
-                      className="previewImage mb-5 flex justify-center rounded-full"
-                      key={index}
-                    >
-                      <ImageWithFallback
-                        className="mx-auto mb-6 rounded-full"
-                        src={upFile.preview}
-                        alt="preview"
-                        height={205}
-                        width={205}
-                        fallbackSrc="/images/fallback-1.png"
-                      />
-                    </div>
-                  );
-                })
-              ) : (
-                <div className="previewImage">
-                  <ImageWithFallback
-                    src={""}
-                    alt="preview"
-                    height={205}
-                    width={205}
-                    className="mx-auto mb-6 rounded-full"
-                    fallbackSrc="/images/fallback-1.png"
-                  />
-                </div>
+        <FormProvider {...methods}>
+          <div className='relative grid grid-cols-6 grid-rows-1'>
+            <Card className='relative col-span-12 h-full !rounded-r-none rounded-l-xl bg-white p-0 pt-10 lg:col-span-4'>
+              {currentStep === 1 && <AddAthlete />}
+              {currentStep === 2 && (
+                <AddGeneralDetails finalFormSubmissionHandler={finalFormSubmissionHandler} />
               )}
-              <div className="mb-14 flex justify-center">
-                <FileUpload onDropCallback={onDropCallback} />{" "}
+            </Card>
+            <Card className='col-span-2 hidden !rounded-l-none rounded-r-xl bg-stone-100 px-7 lg:block'>
+              <div className='mb-10 font-heading text-2xl font-medium uppercase'>Athlete Image</div>
+
+              <div>
+                {preview.length ? (
+                  preview.map((upFile, index) => {
+                    return (
+                      <div
+                        className='previewImage mb-5 flex justify-center rounded-full'
+                        key={index}
+                      >
+                        <ImageWithFallback
+                          className='mx-auto mb-6 rounded-full'
+                          src={upFile.preview}
+                          alt='preview'
+                          height={205}
+                          width={205}
+                          fallbackSrc='/images/fallback-1.png'
+                        />
+                      </div>
+                    )
+                  })
+                ) : (
+                  <div className='previewImage'>
+                    <ImageWithFallback
+                      src={formData.image || ''}
+                      alt='preview'
+                      height={205}
+                      width={205}
+                      className='mx-auto mb-6 rounded-full'
+                      fallbackSrc='/images/fallback-1.png'
+                    />
+                  </div>
+                )}
               </div>
-            </div>
-            <div>
-              <div className="mb-5 font-bold">Note</div>
-              <ul className="list-disc pl-5 text-gray-500">
-                <li>Please upload jpg, png, .tiff file formats only</li>
-                <li>Maximum Size 100 MB</li>
-                <li>Minimum dimension 500px width by 500px height</li>
-              </ul>
-            </div>
-          </Card>
-        </div>
+              <div className='mb-14 flex justify-center'>
+                <FileUpload onDrop={onDropCallback} />
+              </div>
+            </Card>
+          </div>
+        </FormProvider>
       </FormContext.Provider>
     </div>
-  );
+  )
 }
